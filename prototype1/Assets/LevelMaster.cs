3564d8728f193625f4bfd138262c61fe626a3b74
Castipher McSkimming <mcsca959@student.otago.ac.nz>
2021-01-21 14:32:32 +1300

    prototype of two scenes - loading vs flowing

---
+++ prototype1/Assets/LevelMaster.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelMaster : MonoBehaviour
+{
+
+    // Variables referencing two edge colliders
+    public EdgeCollider2D leftWall;
+    public EdgeCollider2D rightWall;
+
+    // Use this for initialization
+    void Start()
+    {
+        // Get the width and height of the camera (in pixels)
+        float screenW = Camera.main.pixelWidth;
+        float screenH = Camera.main.pixelHeight;
+
+        // Create an array consisting of two Vector2 object
+        Vector2[] edgePoints = new Vector2[2];
+
+        // Convert screen coordinates point (0,0) to world coordinates
+        Vector3 leftBottom = Camera.main.ScreenToWorldPoint(new Vector3(0f, 0f, 0f));
+        // Convert screen coordinates point (0,H) to world coordinates
+        Vector3 leftTop = Camera.main.ScreenToWorldPoint(new Vector3(0f, screenH, 0f));
+
+        // Set the two points in the array to screen left bottom
+        // and screen left top points            
+        edgePoints[0] = leftBottom;
+        edgePoints[1] = leftTop;
+
+        // Position the left wall edge collider
+        // at the left edge of the camera
+        leftWall.points = edgePoints;
+
+        // Convert screen coordinates point (W,0) to world coordinates
+        Vector3 rightBottom = Camera.main.ScreenToWorldPoint(new Vector3(screenW, 0f, 0f));
+        // Convert screen coordinates point (W,H) to world coordinates
+        Vector3 rightTop = Camera.main.ScreenToWorldPoint(new Vector3(screenW, screenH, 0f));
+
+        // Set the two points in the array to screen right bottom
+        // and screen right top points            
+        edgePoints[0] = rightBottom;
+        edgePoints[1] = rightTop;
+
+        // Position the left wall edge collider
+        // at the left edge of the camera
+        rightWall.points = edgePoints;
+    }
+}
---
+++ prototype1/Assets/Player.cs
@@ -0,0 +1,94 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Player : MonoBehaviour
+{
+
+    public KeyCode moveLeft;
+    public KeyCode moveRight;
+    public KeyCode moveUp;
+    public KeyCode moveDown;
+
+    public float speed;
+
+    bool atLeftWall = false;
+
+    // Flag indicating whether the player is at the 
+    // right edge of the screen
+    bool atRightWall = false;
+
+    // On collision with a trigger collider...
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        // Check the tag of the object the player
+        // has collided with
+        if (other.tag == "LeftWall")
+        {
+            // If collided with the left wall, set
+            // the left wall flag to true
+            atLeftWall = true;
+        }
+        else if (other.tag == "RightWall")
+        {
+            SceneManager.LoadScene("background2");
+            // If collided with the right wall, set
+            // the right wall flag to true
+
+        }
+    }
+
+    // When no longer colliding with an object...
+    void OnTriggerExit2D(Collider2D other)
+    {
+        // Check the tag of the object the player
+        // has ceased to collide with
+        if (other.tag == "LeftWall")
+        {
+            // If collided with the left wall, set
+            // the left wall flag to true
+            atLeftWall = false;
+        }
+        else if (other.tag == "RightWall")
+        {
+            // If collided with the right wall, set
+            // the right wall flag to true
+            atRightWall = false;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float delta = speed * Time.deltaTime;
+
+        if (Input.GetKey(moveRight))
+        {
+            // Move to the right
+            transform.Translate(new Vector3(speed * delta, 0, 0));
+        }
+        else if (Input.GetKey(moveLeft))
+        {
+            // Move to the left
+            transform.Translate(new Vector3(-speed * delta, 0, 0));
+        }
+        else if (Input.GetKey(moveUp))
+        {
+            //Rotate counterclockwise
+            transform.Rotate(new Vector3(0, 0, speed * delta * 10));
+        }
+        else if (Input.GetKey(moveDown))
+        {
+            //Rotate clockwise
+            transform.Rotate(new Vector3(0, 0, -speed * delta * 10));
+        }
+
+          // If close to wall and moving towards it,
+      // stop the movment
+      if(atLeftWall)  {
+         speed = 0;
+      }
+      if(atRightWall)  {
+            SceneManager.LoadScene("background2");
+      }
+    }
+}
