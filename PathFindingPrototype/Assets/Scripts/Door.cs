d2ea0f390db9e91b529083ba43f3d161f90e638d
Sean Cartman <casea836@student.otago.ac.nz>
2021-01-29 13:04:03 +1300

    Separated questions into there own scenes and Moved in  the Astar labs

---
+++ PathFindingPrototype/Assets/Scripts/Door.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class Door : MonoBehaviour
+{
+    private NavMeshAgent agent;
+    // Start is called before the first frame update
+    void Start()
+    {
+        agent = GetComponent<NavMeshAgent>();
+        agent.updateRotation = false;
+        agent.updateUpAxis = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
