c91ff6e32a317d3109e0c7535564fbe6fbe27130
Samuel Gentry <gensa844@student.otago.ac.nz>
2021-02-03 21:35:53 +1300

    Used anim controller to animate big jims legs

--- Voodoo Island (alpha)/Assets/Scripts/Player/Player.cs
+++ Voodoo Island (alpha)/Assets/Scripts/Player/Player.cs
@@ -5,6 +5,10 @@ using UnityEngine.AI;
 
 public class Player : MonoBehaviour
 {
+
+    // Reference to animator component
+    Animator anim;
+
     //set the starting ammo
     public int ammo = 6;
 
@@ -74,6 +78,9 @@ public class Player : MonoBehaviour
         agent = GetComponent<NavMeshAgent>();
         agent.updateRotation = false;
         agent.updateUpAxis = false;
+
+        // Initialise the reference to the Animator component
+        anim = GetComponent<Animator>();
     }
 
     // Update is called once per frames
@@ -86,6 +93,25 @@ public class Player : MonoBehaviour
         //get movement input
         float GetX = Input.GetAxis("Horizontal");
         float GetY = Input.GetAxis("Vertical");
+
+        //Animation code
+        
+        if(GetX < 0)
+        {
+            anim.SetTrigger("Left");   
+        }
+        else if (GetX > 0)
+        {
+            anim.SetTrigger("Right");
+        }
+        else if(GetY != 0)
+        {
+            anim.SetTrigger("Forward");
+        }
+        else
+        {
+            anim.SetTrigger("Idle");
+        }
 
 
         Vector3 movement = new Vector3(GetX*speed+ 0.0005f,GetY*speed, 0);
