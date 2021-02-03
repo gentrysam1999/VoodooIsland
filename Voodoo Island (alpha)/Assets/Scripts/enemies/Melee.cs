db3968dd4f4e0ab711c08589412cb0c9f3f3fe3a
Sean Cartman <casea836@student.otago.ac.nz>
2021-02-04 11:32:44 +1300

    started melee script

---
+++ Voodoo Island (alpha)/Assets/Scripts/enemies/Melee.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Melee : MonoBehaviour
+{
+
+    public int damage = 1;
+    private bool inRange = false;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void OnCollisionEnter2D(Collision2D other)
+    {
+        //if the player runs into a door and has a key remove the door
+        if (other.gameObject.tag == "Player")
+        {
+            inRange = true;
+        }
+    }
+
+    void OnCollisionExit2D(Collision2D other)
+    {
+        if (other.gameObject.tag == "Player")
+
+    }
+}
