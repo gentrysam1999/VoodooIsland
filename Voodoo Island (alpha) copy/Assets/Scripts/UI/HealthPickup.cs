615151d9254daa801ee6a30876410e6000978da0
Sam Gentry <gensa844@student.otago.ac.nz>
2021-02-05 00:20:10 +1300

    testing out if git will remove settings with a new project

---
+++ Voodoo Island (alpha) copy/Assets/Scripts/UI/HealthPickup.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HealthPickup : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    public GameObject InteractKey;
+
+    private void Start()
+    {
+        InteractKey.SetActive(false);
+    }
+
+    void OnTriggerStay2D(Collider2D other)
+    {
+        // Check the tag of the object the player
+        // has collided with
+        if (other.tag == "Player")
+        {
+            InteractKey.SetActive(true); // false to hide, true to show
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.tag == "Player")
+        {
+            InteractKey.SetActive(false);
+        }
+    }
+}
---
+++ Voodoo Island (alpha) copy/Assets/Scripts/enemies/ChaseNav.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class ChaseNav : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public GameObject target;
+    private NavMeshAgent agent;
+    private bool stop;
+
+
+    void Start()
+    {
+        agent = GetComponent<NavMeshAgent>();
+        agent.updateRotation = false;
+        agent.updateUpAxis = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        EnemySight enemySight = GetComponent<EnemySight>();
+        if (enemySight.isActive())
+        {
+            agent.SetDestination(target.transform.position);
+            Navigate.DebugDrawPath(agent.path.corners);
+        }
+        
+        //Debug.Log(agent.velocity.ToString());
+    }
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        //Bullet b = other.GetComponent<Bullet>();
+        if (other.tag == "Bullet")
+        {
+            Destroy(gameObject);
+        }
+    }
+
+}
