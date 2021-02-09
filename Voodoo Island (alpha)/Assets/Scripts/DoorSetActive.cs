272ce2d2bbad9c32b863e80594eaa6592ad99b63
C Mcskimming <mcsca959@student.otago.ac.nz>
2021-02-09 13:42:55 +1300

    tried to add navmesh obstacle to door, player still goes through

--- Voodoo Island (alpha)/Assets/Scripts/DoorSetActive.cs
+++ Voodoo Island (alpha)/Assets/Scripts/DoorSetActive.cs
@@ -2,25 +2,36 @@
 using System.Collections.Generic;
 using UnityEngine.SceneManagement;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class DoorSetActive : MonoBehaviour, IDoor
 {
     private Animator animator;
     private bool isOpen = false;
 
+    private NavMeshObstacle navmeshobstacle;
+
     private void Awake()
     {
         animator = GetComponent<Animator>();
+        navmeshobstacle = GetComponent<NavMeshObstacle>();
+    }
+
+    private void Start()
+    {
+        navmeshobstacle.enabled = true;
     }
     //if the door is toggled to open the door animator will activate.
     public void OpenDoor()
     {
+        navmeshobstacle.enabled = false;
         animator.SetBool("Open", true);
     }
 
     //if the door is toggled to close the animator will go back to idle.
     public void CloseDoor()
     {
+        navmeshobstacle.enabled = true;
         animator.SetBool("Open", false);
     }
 
