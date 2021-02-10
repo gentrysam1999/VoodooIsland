7543898025a17d1f66db35a3497b0019f401376e
C Mcskimming <mcsca959@student.otago.ac.nz>
2021-02-11 11:54:20 +1300

    teleporting between areas, witch moved to jetty

---
+++ Voodoo Island (beta)/Assets/Scripts/Teleport.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+using UnityEngine.SceneManagement;
+
+public class Teleport : MonoBehaviour
+{
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        //Check if the player is on a key and allow the player to pick up a key
+        if (other.tag == "Hall1")
+        {
+            SceneManager.LoadScene("Area2");
+        }
+        else if (other.tag == "Living")
+        {
+            SceneManager.LoadScene("Area3");
+        }
+        else if (other.tag == "Jetty")
+        {
+            SceneManager.LoadScene("Area4");
+        }
+    }
+}
