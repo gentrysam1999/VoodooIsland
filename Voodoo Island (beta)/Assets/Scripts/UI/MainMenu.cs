c1fc825ed919ae51e71b30f1c867dea5d7d7c1e6
Jacob Cone <conja458@student.otago.ac.nz>
2021-02-11 14:48:04 +1300

    added main menu to game. working on credits scene

---
+++ Voodoo Island (beta)/Assets/Scripts/UI/MainMenu.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+
+    public void StartGame()
+    {
+        // Load the first area
+        SceneManager.LoadScene("Area1");
+    }
+
+    public void Credits()
+    {
+        SceneManager.LoadScene("Credits");
+    }
+
+    public void QuitGame()
+    {
+        // Quit the application
+        Application.Quit();
+    }
+}
