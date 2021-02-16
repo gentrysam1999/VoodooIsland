f49ea1eb316e1b656f35d6060ea8512f2174bf3c
Jacob Cone <conja458@student.otago.ac.nz>
2021-02-16 13:39:01 +1300

    added Finish script

---
+++ Voodoo Island (beta)/Assets/Scripts/UI/Finish.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class Finish : MonoBehaviour
+{
+    public Text stats;
+    // Start is called before the first frame update
+    void Start()
+    {
+        int ammo = GlobalControl.Instance.Ammo;
+        int health = GlobalControl.Instance.HP;
+        int needle = GlobalControl.Instance.needle;
+        bool hasDoll = GlobalControl.Instance.hasDoll;
+        float speed = GlobalControl.Instance.speed;
+        int bulletsFired = GlobalControl.Instance.bulletsFired;
+
+        string stat = "YOU WIN\n\n";
+        stat += "Big Jim found his parents, trapped in the nursery. \nHaving freed them, Jim burned the mansion to the ground, laying the voodoo curse to rest... for now!\n";
+        stat += "YOU FIRED: " + bulletsFired + " BULLETS";
+        stat += "YOU PICKED UP: " + needle + " NEEDLES\n";
+
+        stats.text = stat;
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
