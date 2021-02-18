9bde7294ad045173b7801df183ab7ab8165e6956
Jacob Cone <conja458@student.otago.ac.nz>
2021-02-19 12:13:30 +1300

    Changed UI layout for intro and end

--- Voodoo Island (beta)/Assets/Scripts/UI/Intro.cs
+++ Voodoo Island (beta)/Assets/Scripts/UI/Intro.cs
@@ -14,7 +14,7 @@ public class Intro : MonoBehaviour
     {
         string message1 = "After months of radio silence from his family, our heroic police officer, Big Jim, finally returned home to his family's island to figure out why they had cut all contact with him.\n\n However, instead of finding his family, Big Jim was quickly confronted by a mysterious presence which had taken over control of his family home.\n\n";
         message1 += "Isolated and cut off from civilization, Big Jim now has no choice but to plunge forwards into his old home, in an effort to save those he cares about from the secrets of the Voodoo Mansion...\n\n";
-        string message2 = "\npress enter to continue";
+        string message2 = "\nPRESS ANY KEY TO CONTINUE";
 
         statsText.text = message1;
         statsText2.text = message2;
@@ -23,7 +23,7 @@ public class Intro : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKey("return"))
+        if (Input.anyKeyDown)
         {
             SceneManager.LoadScene("Area1");
         }
