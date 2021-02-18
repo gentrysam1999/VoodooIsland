155bfa9e8a9b5fd32af5e14ffca9aa13ffbc495b
Jacob Cone <conja458@student.otago.ac.nz>
2021-02-19 10:26:42 +1300

    Changed layout and text of intro screen a little

--- Voodoo Island (beta)/Assets/Scripts/UI/Intro.cs
+++ Voodoo Island (beta)/Assets/Scripts/UI/Intro.cs
@@ -12,7 +12,8 @@ public class Intro : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        string message1 = "After months of radio silence from his family, our heroic police officer, Big Jim finally returned home to his family's island to figure out why they had cut all contact with him. However instead of finding his family, Big Jim was confronted by a mysterious presence which had taken over control of his family home.";
+        string message1 = "After months of radio silence from his family, our heroic police officer, Big Jim, finally returned home to his family's island to figure out why they had cut all contact with him.\n\n However, instead of finding his family, Big Jim was quickly confronted by a mysterious presence which had taken over control of his family home.\n\n";
+        message1 += "Isolated and cut off from civilization, Big Jim now has no choice but to plunge forwards into his old home, in an effort to save those he cares about from the secrets of the Voodoo Mansion...\n\n";
         string message2 = "\npress enter to continue";
 
         statsText.text = message1;
