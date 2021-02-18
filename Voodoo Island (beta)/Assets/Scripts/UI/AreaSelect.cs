6468163c74ffc4e11beb7eccd7e74afdafb251b2
Samuel Gentry <gensa844@student.otago.ac.nz>
2021-02-19 12:46:11 +1300

    Created a level select that will track the levels that you've been to

---
+++ Voodoo Island (beta)/Assets/Scripts/UI/AreaSelect.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class AreaSelect : MonoBehaviour
+{
+    private Button button;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        button = GetComponent<Button>();
+
+        if(gameObject.name == "Button1")
+        {
+            button.interactable = GlobalControl.Instance.level >= 0;
+        }
+        else if (gameObject.name == "Button2")
+        {
+            button.interactable = GlobalControl.Instance.level >= 1;
+        }
+        else if (gameObject.name == "Button3")
+        {
+            button.interactable = GlobalControl.Instance.level >= 2;
+        }
+        else if (gameObject.name == "Button3")
+        {
+            button.interactable = GlobalControl.Instance.level >= 3;
+        }
+    }
+
+    
+}
