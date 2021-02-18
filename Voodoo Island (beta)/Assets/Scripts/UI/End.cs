d9c07a66e974ac65044bb12257231a223c021e52
Jacob Cone <conja458@student.otago.ac.nz>
2021-02-19 12:46:21 +1300

    changed it so that you shouldn't be able to leave the final scene by mashing the left click because the player might miss it

--- Voodoo Island (beta)/Assets/Scripts/UI/End.cs
+++ Voodoo Island (beta)/Assets/Scripts/UI/End.cs
@@ -22,7 +22,7 @@ public class End : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.anyKeyDown)
+        if (Input.anyKeyDown && !Input.GetButtonDown("Fire1"))
         {
             SceneManager.LoadScene("Finish");
         }
