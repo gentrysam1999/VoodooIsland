4bfdd51510dcbd6b9b6ee861714ddbf80f5fb8d3
Jacob Cone <conja458@student.otago.ac.nz>
2021-02-18 16:40:33 +1300

    Changed scale of main menu and final screen. also prevented bug that modified the number of needles / health you had on new start of game if you went from finish screen to main menu to the beginning again

--- Voodoo Island (beta)/Assets/Scripts/UI/MainMenu.cs
+++ Voodoo Island (beta)/Assets/Scripts/UI/MainMenu.cs
@@ -4,10 +4,13 @@ using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour
 {
-
     public void StartGame()
     {
         // Load the first area
+        int needle = 0;
+        GlobalControl.Instance.needle = needle;
+        int health = 5;
+        GlobalControl.Instance.HP = health;
         SceneManager.LoadScene("Area1");
     }
 
